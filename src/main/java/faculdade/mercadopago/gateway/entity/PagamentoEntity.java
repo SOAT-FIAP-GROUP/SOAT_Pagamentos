667c1dc10380ee21d7a1668930375bb02bc5b35d
JoseAugustoDosSantos <augusto03jose@gmail.com>
2025-12-16 21:57:10 -0300

    incluindo Ãcenario bdd

--- src/main/java/faculdade/mercadopago/gateway/entity/PagamentoEntity.java
+++ src/main/java/faculdade/mercadopago/gateway/entity/PagamentoEntity.java
@@ -1,4 +1,5 @@
 package faculdade.mercadopago.gateway.entity;
+
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
--- src/main/java/faculdade/mercadopago/usecase/IPagamentoUseCase.java
+++ src/main/java/faculdade/mercadopago/usecase/IPagamentoUseCase.java
@@ -4,10 +4,8 @@ import faculdade.mercadopago.controller.mapper.dto.request.QrCodeRequest;
 import faculdade.mercadopago.entity.Pedido;
 import faculdade.mercadopago.entity.pagamento.ConfirmacaoPagamentoRes;
 import faculdade.mercadopago.entity.pagamento.QrCodeRes;
-import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
-import java.util.ConcurrentModificationException;
 
 public interface IPagamentoUseCase {
     QrCodeRes processarQrCode(QrCodeRequest request);
